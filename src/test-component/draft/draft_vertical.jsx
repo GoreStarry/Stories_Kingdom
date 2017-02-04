2dcc16b3e147c563586208e9f7e0952396fa4105
Gore Wang <sunrise91.t3@gmail.com>
2017-02-04 13:22:36 +0800

    basic vertical-rl done

--- src/test-component/draft/draft_vertical.jsx
+++ src/test-component/draft/draft_vertical.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import { Editor, EditorState } from 'draft-js';
+
 // import './draft.scss';
 import styles from './draft.scss';
 
 class DraftVertical extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty()
+    };
+    this.onChange = (editorState) => this.setState({
+      editorState
+    });
+  }
+
   render() {
     return (
-      <div className={ styles.test }>
-        test
-        <p>test2</p>
+      <div id="outer" className={ styles.test }>
+        <Editor className="Draft" editorState={ this.state.editorState } onChange={ this.onChange } />
       </div>
       );
   }
