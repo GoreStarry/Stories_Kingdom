c9c1e753474e0b4a7bc3b8efe0597a4d819c7b46
Gore Wang <sunrise91.t3@gmail.com>
2017-04-04 16:45:54 +0800

    contentState debounce auto update by RxJS

---
+++ src/containers/Stage-Editor/helpers/getArticleContentState.js
@@ -0,0 +1,25 @@
+import { getStoryEditRecordLocal } from '../../../helpers/Local-Edit-Record/getStoryEditRecordLocal';
+
+/**
+   * try to get contentState from localStorage or redux or just new one
+   */
+export const getArticleDraftContent = (story_id, article_id, articles) => {
+
+
+  return getLocalDraftContent(story_id, article_id) || getReduxStoreDraftContent(story_id, article_id, articles)
+}
+
+
+function getLocalDraftContent(story_id, article_id) {
+  try { // check localStorage exist cache contentState or not
+    return contentState = getStoryEditRecordLocal(story_id).update_cache.find((cache) => {
+      return cache.article_id === article_id;
+    }).contentState;
+  } catch (error) {
+    return false
+  }
+}
+
+function getReduxStoreDraftContent(story_id, article_id, articles) {
+  return JSON.parse(articles[story_id][article_id].draftContent);
+}
